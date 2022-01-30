41d38bf3bfddaf35666bbc5d898dc2c36071cdf1
Jonas_Jones <91549607+J-onasJones@users.noreply.github.com>
2022-01-30 21:01:29 +0000

    Uploaded cmd.py v0.1

---
+++ cmd-v0.1.py
@@ -0,0 +1,28 @@
+from mcstatus import MinecraftServer
+from time import sleep
+
+ip = input("Please Enter server IP: ")
+port = input("Enter Port (press enter for default port 25565): ")
+
+#temporary variable def
+ip = "simp.ztereohype.codes"
+
+if port == "":
+    port = "25565"
+
+server = MinecraftServer.lookup(ip + ":" + port)
+
+
+while True:
+    status = server.status()
+    query = server.query()
+    print("---------- " + ip + ":" + port + " ----------")
+    print(f"version: v{status.version.name} (protocol {status.version.protocol})")
+    print(f'description: "{status.description}"')
+
+    print(f"host: {query.raw['hostip']}:{query.raw['hostport']}")
+    print(f"software: v{query.software.version} {query.software.brand}")
+    print(f"plugins: {query.software.plugins}")
+    print(f"players: {status.players.online}/{status.players.max}")
+    print(f"Players Online: {query.players.names}")
+    print("\n")
